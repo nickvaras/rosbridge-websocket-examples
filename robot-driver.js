94cd4fcaec36a181b325552340105ef40a9b11f7
nickvaras <34077897+nickvaras@users.noreply.github.com>
2018-10-02 13:33:54 -0400

    Create robot-driver.js
    This example builds upon previous examples and adds the ability to stop the robot when navigating to a goal, as well as the ability to query the current location of the robot in its internal map.

---
+++ robot-driver.js
@@ -0,0 +1,71 @@
+const WebSocket = require('ws');   // sudo npm i ws
+const ws = new WebSocket('ws://35.188.228.188:9090');
+
+ws.robotLocation = {x: null, y: null};
+
+ws.on('open', function open() {
+    console.log('New connection');    
+});
+
+ws.goToWaypoint = function(waypointName){
+    let request_coordinates_msg ={"op":"call_service","service":"/waypoint_db/retrieve_waypoint","args":{"mapName":"WebGen","waypointName": waypointName}};
+    ws.send(JSON.stringify(request_coordinates_msg));
+};
+
+ws.on('message', function(msg){
+    let received_message  = JSON.parse(msg);
+    switch(received_message.op){
+        case 'service_response':
+            ws.publishGoal(msg);
+            break;
+        case 'publish':
+            ws.robotLocation.y = received_message.msg.position.y;
+            ws.robotLocation.x = received_message.msg.position.x;
+            Console.log('Coordinates received. console.log(ws.robotLocation):')
+            Console.log(ws.robotLocation);
+            ws.queryPosition(false);
+    }
+});
+
+ws.on('close', function() {
+    console.log('closing connection');
+    ws.close();
+});
+
+ws.publishGoal = function(msg){
+    console.log(msg);
+    waypoint_coordinates  = JSON.parse(msg);
+    x_coordinate = waypoint_coordinates.values.response.x;
+    y_coordinate = waypoint_coordinates.values.response.y;
+    z_coordinate = waypoint_coordinates.values.response.z;
+    w_coordinate = waypoint_coordinates.values.response.w;
+    let pose_message =  {"op": "publish",
+                        "topic": "/move_base_navi_simple/goal",
+                        "msg":{ "header": {"frame_id": "map"},
+                            "pose": {"position": {"x": x_coordinate, "y": y_coordinate}, "orientation": {"z": z_coordinate, "w": w_coordinate}}}
+                        }; 
+    ws.send(JSON.stringify(pose_message));
+}
+
+ws.cancelGoal = function(){
+    console.log('trying to cancel goal');
+    let pose_message =  {"op": "publish",
+    "topic": "/move_base_navi/cancel",
+    "msg":{ "stamp": "",
+            "id": ""}
+    }; 
+    ws.send(JSON.stringify(pose_message));
+}
+
+ws.queryPosition = function(onOff){
+    let op = "subscribe";
+    if(onOff===false){
+        op="unsubscribe";
+    }
+    let pose_subscribe_message =  {"op": op,
+        "topic": "/robot_pose",
+        };  
+    ws.send(JSON.stringify(pose_subscribe_message));
+}
+
+module.exports = ws;
