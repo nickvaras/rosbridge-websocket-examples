f60881352732eb4c4889b967fb8bd886c3e87602
Nicolas Varas <nickv@waypointrobotics.com>
2018-10-23 11:51:44 -0400

    added position and waypoint coordinates queries

--- driver_use_example.py
+++ driver_use_example.py
@@ -1,13 +1,34 @@
 from robot_driver import MyRosbridgeClient
-from time import sleep
+import time
 
 try:
     my_websocket_client = MyRosbridgeClient('ws://35.188.228.188:9090/')
     my_websocket_client.connect()
-    #my_websocket_client.advertise_topic()              
-    #my_websocket_client.go_to_named_waypoint('Start')
-    my_websocket_client.start_mission('start_nayan_x_3')
-    sleep(2)  
-    # my_websocket_client.run_forever()
+
+
+    # This is an example script, uncomment relevant lines to try different functions
+
+    """ Sending the robot to a waypoint:"""
+    my_websocket_client.go_to_named_waypoint('Start')
+    
+    """ Starting a mission/program"""
+    #my_websocket_client.start_mission('start_nayan_x_3')
+    
+
+    """ Current approximate robot position can be queried directly like this: """
+    #print(my_websocket_client.robot_position_x)
+    #print(my_websocket_client.robot_position_y)
+    
+    """ Use this method to stop any running playlist/mission/program """
+    #my_websocket_client.stop_mission()    
+
+    """ Use this method to get the x-y coordinates of a saved waypoint """
+    #my_websocket_client.get_waypoint_coordinates('Start')
+    
+    """ If you want this script to run forever, uncomment this """
+    #my_websocket_client.run_forever()
+    # or
+    time.sleep(5)
+
 except KeyboardInterrupt:
     my_websocket_client.close()
--- robot_driver.py
+++ robot_driver.py
@@ -2,15 +2,27 @@ from json import dumps, loads
 # 'pip install ws4py' or 'sudo apt-get install python-ws4py' .See: https://ws4py.readthedocs.io/en/latest/sources/install/
 from ws4py.client.threadedclient import WebSocketClient
 from math import sin, cos
+from time import sleep
+
 
 class MyRosbridgeClient(WebSocketClient):
 
+    def __init__(self, address_port):
+        WebSocketClient.__init__(self, address_port)
+        self.robot_position_x = None
+        self.robot_position_y = None
+        self.waypoint_coordinates_x = None
+        self.waypoint_coordinates_y = None
+        self.coordinates_message_received = False
+        self.move_to_waypoint_when_coordinates_received = False
+
     def opened(self):
         print "Connection opened..."
+        self.subscribe_to_robot_pose()
 
     def advertise_topic(self):
         msg = {'op': 'advertise', 'topic': '/move_base_simple/goal',
-            'type': 'geometry_msgs/PoseStamped'}
+               'type': 'geometry_msgs/PoseStamped'}
         self.send(dumps(msg))
 
     def closed(self, code, reason=None):
@@ -18,38 +30,31 @@ class MyRosbridgeClient(WebSocketClient):
 
     def move_to(self, x, y, z, w):
         msg = {'op': 'publish',
-        'topic': '/move_base_navi_simple/goal',
-        'msg':
-        {
-        'header': {'frame_id': 'map'},
-        'pose': {"position": {'x': x, 'y': y}, 'orientation': {'z': z, 'w': w}}
-        }}
+               'topic': '/move_base_navi_simple/goal',
+               'msg':
+               {
+                   'header': {'frame_id': 'map'},
+                   'pose': {"position": {'x': x, 'y': y}, 'orientation': {'z': z, 'w': w}}
+               }}
         self.send(dumps(msg))
 
-    def go_to_named_waypoint(self,waypoint_name):
-        msg = { "op": "call_service",
-        "service": '/waypoint_db/retrieve_waypoint',
-        "args": 
-        {
-        "mapName":'WebGen',
-        "waypointName":waypoint_name
-        }}
-        self.send(dumps(msg))
+    def go_to_named_waypoint(self, waypoint_name):
+        self.move_to_waypoint_when_coordinates_received = True
+        self.request_waypoint_coordinates(waypoint_name)
 
     def cancel_goal(self):
-        msg = { "op": "publish",
-        "topic": '/move_base_navi/cancel',
-        "msg": 
-        {
-        "stamp":"",
-        "id":""}}
+        msg = {"op": "publish",
+               "topic": '/move_base_navi/cancel',
+               "msg":
+               {
+                   "stamp": "",
+                   "id": ""}}
         self.send(dumps(msg))
-    
+
     def received_message(self, msg):
-        print msg
-        #waypoint_coordinates  = loads(str(msg))
-        message  = loads(str(msg))
-	if message['op'] == 'service_response':
+        #print msg
+        message = loads(str(msg))
+        if message['op'] == 'service_response':
             if message['service'] == '/mission_control/stop_mission_file':
                 print('Mission Stopped')
             elif message['service'] == '/waypoint_db/retrieve_waypoint':
@@ -57,28 +62,48 @@ class MyRosbridgeClient(WebSocketClient):
                 y_coordinate = message['values']['response']['y']
                 z_coordinate = message['values']['response']['z']
                 w_coordinate = message['values']['response']['w']
-                self.move_to(x_coordinate,y_coordinate,z_coordinate,w_coordinate)
+                self.waypoint_coordinates_x = x_coordinate
+                self.waypoint_coordinates_y = y_coordinate
+                self.coordinates_message_received = True                
+                if self.move_to_waypoint_when_coordinates_received:
+                    self.move_to_waypoint_when_coordinates_received = False
+                    self.move_to(x_coordinate, y_coordinate, z_coordinate, w_coordinate)
+
         elif message['op'] == 'publish':
-            pass
-	"""
-		case 'publish':
-		    ws.robotLocation.y = received_message.msg.position.y;
-		    ws.robotLocation.x = received_message.msg.position.x;
-		    Console.log('Coordinates received. console.log(ws.robotLocation):')
-		    Console.log(ws.robotLocation);
-		    ws.queryPosition(false);
-	    }
-	});
-	"""
-    def start_mission(self,mission_name):
-        msg = { "op": "call_service",
-        "service": "/mission_control/run_mission_from_file",
-        "args": {"request":mission_name }}
+            # update the robot position
+            self.robot_position_x = message['msg']['position']['x']
+            self.robot_position_y = message['msg']['position']['y']
+
+    def start_mission(self, mission_name):
+        msg = {"op": "call_service",
+               "service": "/mission_control/run_mission_from_file",
+               "args": {"request": mission_name}}
         self.send(dumps(msg))
 
     def stop_mission(self):
-        msg = { "op": "call_service",
-        "service": "/mission_control/stop_mission_file",
-        "args": {"request":"" }}
+        msg = {"op": "call_service",
+               "service": "/mission_control/stop_mission_file",
+               "args": {"request": ""}}
         self.send(dumps(msg))
 
+    def subscribe_to_robot_pose(self):
+        msg = {"op": "subscribe", "topic": "/robot_pose"}
+        self.send(dumps(msg))
+
+    def request_waypoint_coordinates(self, waypoint_name):
+        msg = {"op": "call_service",
+               "service": '/waypoint_db/retrieve_waypoint',
+               "args":
+               {
+                   "mapName": 'WebGen',
+                   "waypointName": waypoint_name
+               }}
+        self.send(dumps(msg))
+
+    def get_waypoint_coordinates(self, waypoint_name):
+        self.request_waypoint_coordinates(waypoint_name)
+        while not self.coordinates_message_received:
+            sleep(0.15)
+        self.coordinates_message_received = False
+        return self.waypoint_coordinates_x, self.waypoint_coordinates_y
+        
